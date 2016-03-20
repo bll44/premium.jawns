6965b9d430a21397d83aa2b3e4ab86be43a918b0
Brady Latsha <brady.latsha@gmail.com>
2016-03-20 03:23:33 +0000

    updating 3rd party framework files

--- resources/views/_layouts/header.blade.php
+++ resources/views/_layouts/header.blade.php
@@ -1,11 +1,13 @@
 <head>
 	<title>Premium Jawns</title>
-	<link type="text/css" href="{{ asset('css/custom.css') }}" rel="stylesheet">
-	<link type="text/css" href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+	<link type="text/css" href="{{ asset('bootstrap-3.3.6-dist/css/bootstrap.css') }}" rel="stylesheet">
+	<link type="text/css" href="{{ asset('bootstrap-3.3.6-dist/css/bootstrap-theme.css') }}" rel="stylesheet">
 	<link type="text/css" href="{{ asset('css/scrolling-nav.css') }}" rel="stylesheet">
+	<link type="text/css" href="{{ asset('font-awesome-4.5.0/css/font-awesome.min.css') }}" rel="stylesheet">
+	<link type="text/css" href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
-	<script type="text/javascript" src="{{ asset('js/jquery-2.2.1.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/jquery-2.2.2.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('bootstrap-3.3.6-dist/js/bootstrap.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/jquery.easing.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/scrolling-nav.js') }}"></script>
 </head>
