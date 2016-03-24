c3eb6bafb775fede2e7496c94ed84e149ca4caa1
Brady Latsha <brady.latsha@gmail.com>
2016-03-24 19:39:54 +0000

    add track modal work

--- resources/views/curation/add_track_modal.blade.php
+++ resources/views/curation/add_track_modal.blade.php
@@ -4,15 +4,19 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="addTrackModalTitle">Modal title</h4>
+				<h4 class="modal-title" id="addTrackModalTitle">Add track</h4>
 			</div>
 			<form action="" method="post">
 				{{ csrf_field() }}
 			<div class="modal-body">
+				<div class="form-group">
+					<label for="soundcloud-url">Soundcloud URL</label>
+					<input type="text" name="soundcloud-url" class="form-control" placeholder="Soundcloud song URL">
+				</div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary">Save changes</button>
+				<button type="submit" class="btn btn-primary">Save changes</button>
 			</div>
 			</form>
 		</div><!-- /.modal-content -->
